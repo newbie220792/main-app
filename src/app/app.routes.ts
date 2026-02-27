2342828fcab17d01b8384225033a1dfa5db93a79
Anthony.Doan <anthony.doan@code88.asia>
2026-02-27 17:49:29 +0700

    dev 2026

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,27 @@
 import {Routes} from '@angular/router';
 import {loadRemoteModule} from '@angular-architects/module-federation';
+import {NotFoundComponent} from '../components/common/not-found/not-found';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: '**',
+    component: NotFoundComponent,
+  },
+  {
+    path: 'sso-app',
     loadComponent: () => loadRemoteModule({
       type: 'module',
-      remoteEntry: 'http://localhost:4200/remoteEntry.js',
+      remoteEntry: 'http://localhost/sso-app/remoteEntry.js',
       exposedModule: './App'
+    }),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => loadRemoteModule({
+      type: 'script',
+      remoteEntry: 'http://localhost:5000/remoteEntry.js',
+      exposedModule: './Dashboard',
+      remoteName: 'vocabularyApp'
     }),
   }
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,12 @@ module.exports = withModuleFederationPlugin({
   name: "shell",
   remotes: {
     // remoteApp: "product@http://localhost:4100/remoteEntry.js",
-    ssoApp: "ssoApp@http://localhost:4200/remoteEntry.js",
+    // ssoApp: "ssoApp@http://localhost:4200/remoteEntry.js",
   },
   shared: {
-    "@angular/core": {singleton: true, strictVersion: true},
-    "@angular/common": {singleton: true, strictVersion: true},
-    "@angular/router": {singleton: true, strictVersion: true},
-    rxjs: {singleton: true, strictVersion: true},
+    "@angular/core": {singleton: true, strictVersion: false},
+    "@angular/common": {singleton: true, strictVersion: false},
+    "@angular/router": {singleton: true, strictVersion: false},
+    rxjs: {singleton: true, strictVersion: false},
   },
-
 });
